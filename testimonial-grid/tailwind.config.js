8d147e49b95514d441424dfd3102300b52645f01
boyeClaude <boye.claude@gmail.com>
2022-08-29 13:05:38 +0000

    Testimonial UI
    - feat: testimonial UI done

---
+++ testimonial-grid/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
+  theme: {
+    extend: {
+      fontFamily: ["Barlow Semi Condensed", "sans-serif"],
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
