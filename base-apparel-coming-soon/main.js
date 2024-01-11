fbea089e36f2011f5141524318f4fc9e9f48eea4
Claude Boye <boye.claude@gmail.com>
2024-01-11 11:48:51 +0000

    Base apparel comming soon
     - checking email from user using js
     - update css and markup file

--- base-apparel-coming-soon/main.js
+++ base-apparel-coming-soon/main.js
@@ -1,9 +1,23 @@
 const email = document.getElementById("email");
 const button = document.querySelector(".btn-submit");
 const warningMessage = document.querySelector(".waring-message");
+const successMessage = document.querySelector(".success-message");
+
+// email validator function
+function ValidateEmail(input) {
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.value.match(validRegex)) {
+    warningMessage.style.display = "none";
+    successMessage.style.display = "block";
+  } else {
+    warningMessage.style.display = "block";
+    email.style.border = "2px solid red";
+    successMessage.style.display = "none";
+  }
+}
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  warningMessage.style.display = "block";
-  console.log("button was clicked");
+  ValidateEmail(email);
 });
