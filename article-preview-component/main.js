53dd588cd19ee8a163e0cfec1262dbb777be3ee4
boyeClaude <boye.claude@gmail.com>
2024-03-04 15:47:17 +0000

    Article preview component UI - feat:UI done(html and css file) added

---
+++ article-preview-component/main.js
@@ -0,0 +1,8 @@
+const authorSection = document.querySelector("#author-section");
+const wrapperSocial = document.querySelector(".wrapper__socials");
+const iconShareButton = document.querySelector(".icon-share");
+const iconHiddenShareButton = document.querySelector("#icon-share_hidden");
+
+iconShareButton.addEventListener("click", () => {
+  console.log("btn clicked !");
+});
